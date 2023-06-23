9011572e71eae9035b79256324198320048f4920
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-06-24 00:24:10 +0900

    Modified base files for authentication

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const Bootstrap = () => {
+  return (
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_URL as string}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID as string}
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </Auth0Provider>
+  );
+};
+
+root.render(<Bootstrap />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
