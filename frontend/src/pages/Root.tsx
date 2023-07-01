2be17f3645c33d56126dd7cb9e6735d4e3616c83
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-07-02 01:41:22 +0800

    Add redirect to Root

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,19 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Header } from "../components/Header";
 import { useEffect } from "react";
 
 export const Root = () => {
-  const { isAuthenticated, isLoading, loginWithRedirect, user } = useAuth0();
+  const { isAuthenticated, isLoading, user } = useAuth0();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (!isLoading && !isAuthenticated) loginWithRedirect();
-  }, [isAuthenticated, isLoading, loginWithRedirect]);
+    if (!isLoading) {
+      console.log(isLoading, isAuthenticated);
+      if (!isAuthenticated) return navigate("/home");
+      navigate("/app");
+    }
+  }, [isAuthenticated, isLoading, navigate]);
 
   return (
-    <div>
+    <>
       <Header user={user} />
       <Outlet />
-    </div>
+    </>
   );
 };
