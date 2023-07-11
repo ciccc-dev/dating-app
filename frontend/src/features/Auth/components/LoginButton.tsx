8e7eff394c4931e116be890faf69ca5055d0e0fe
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-07-12 00:21:47 +0900

    Change Design

--- frontend/src/features/Auth/components/LoginButton.tsx
+++ frontend/src/features/Auth/components/LoginButton.tsx
@@ -1,8 +1,9 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Typography } from "@mui/material";
 
 export const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   const redirectLoginPage = () => loginWithRedirect();
 
-  return <button onClick={redirectLoginPage}>Log In</button>;
+  return <Typography onClick={redirectLoginPage}>LOGIN</Typography>;
 };
