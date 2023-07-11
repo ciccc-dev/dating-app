5f2a60dd439536236572c834c9133361d6163aeb
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-07-11 23:59:15 +0900

    Add theme to Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,41 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button, Container } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { grey, pink } from "@mui/material/colors";
+
+import { Header } from "../components/Header";
+
 export const Home = () => {
-  return <>This is Home</>;
+  const headerTheme = createTheme({
+    palette: {
+      primary: {
+        main: grey[800],
+      },
+      secondary: {
+        main: pink[400],
+      },
+    },
+  });
+
+  const { user } = useAuth0();
+
+  const handleClickCreateAccountButton = () => {
+    console.log("Redirect to signup page");
+  };
+
+  return (
+    <ThemeProvider theme={headerTheme}>
+      <Header user={user} />
+      <Container>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={handleClickCreateAccountButton}
+        >
+          Create account
+        </Button>
+      </Container>
+    </ThemeProvider>
+  );
 };
