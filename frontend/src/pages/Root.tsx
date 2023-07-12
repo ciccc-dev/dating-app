8b0825329006f83bcf89e3d9b6b2c28f08a2a9d3
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-07-12 12:22:41 +0900

    Improved logic of initial load

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
+import { differenceInSeconds, parseISO } from "date-fns";
 import { useAuth0 } from "@auth0/auth0-react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { pink } from "@mui/material/colors";
@@ -15,12 +16,20 @@ const theme = createTheme({
 export const Root = () => {
   const { isAuthenticated, isLoading, user } = useAuth0();
   const navigate = useNavigate();
+  const LOGIN_DURATION = 3;
 
   useEffect(() => {
     if (isLoading) return;
     if (!isAuthenticated) return navigate("/home");
-    navigate("/app");
-  }, [isAuthenticated, isLoading, navigate]);
+
+    if (user?.updated_at) {
+      const secondsAfterLogin = differenceInSeconds(
+        new Date(),
+        parseISO(user?.updated_at)
+      );
+      secondsAfterLogin < LOGIN_DURATION && navigate("/app");
+    }
+  }, [isAuthenticated, isLoading, navigate, user?.updated_at]);
 
   return (
     <>
