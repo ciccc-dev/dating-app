cafd918795c3d62fd6b152518a9d63a7580776e9
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-07-12 17:52:07 +0900

    Adds account part to navigation

--- frontend/src/features/DatingApp/components/Navigation/index.tsx
+++ frontend/src/features/DatingApp/components/Navigation/index.tsx
@@ -1,30 +1,43 @@
 import { SyntheticEvent } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
-import { Tab, Tabs } from "@mui/material";
+import { Tab, Tabs, Typography } from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
+import { styled } from "@mui/system";
 
 const tabValues = ["discovery", "likes", "messages"];
 
 export const DatingAppNavigation = () => {
+  // Prepare variables to set tab from current url path
   const location = useLocation();
   const currentPath = location.pathname.replace("/app/", "");
   const tabIndex = tabValues.indexOf(currentPath);
 
+  const { user } = useAuth0();
+
+  const navigate = useNavigate();
   const handleChange = (e: SyntheticEvent) =>
     e.currentTarget.textContent && navigate(e.currentTarget.textContent);
 
-  const navigate = useNavigate();
+  const MyAccount = () => (
+    <StyledAccountBox>
+      <Typography variant="h5" color="common.white">
+        {user?.name ?? "---"}
+      </Typography>
+    </StyledAccountBox>
+  );
 
   return (
     <div>
+      <MyAccount />
       <Toolbar />
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+      <Box>
         <Tabs
           value={tabIndex}
           onChange={handleChange}
@@ -49,3 +62,10 @@ export const DatingAppNavigation = () => {
     </div>
   );
 };
+
+const StyledAccountBox = styled(Box)`
+  padding: 10px;
+  background-color: #ec407a;
+  text-align: center;
+  border-bottom: 1px;
+`;
