c5bacca98123478ec668c6d1746e9720835f8abb
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-07-13 17:00:14 +0900

    Adds routes

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
+import apiRoutes from "./routes";
 
 const app = express();
 const port = 3000;
+
+app.use("/api", apiRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
