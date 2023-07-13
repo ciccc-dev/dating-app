88cf75c0abf611b0edeac0e56248e71503a8d86f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-07-13 23:47:52 +0900

    Adds cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,23 @@
 import express, { Request, Response } from "express";
+// import cors from "cors";
 import apiRoutes from "./routes";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+import { webscoketConnect } from "./lib/messages/websocketServer";
 
 const app = express();
 const port = 3000;
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+webscoketConnect(io);
 
 app.use("/api", apiRoutes);
 
@@ -10,6 +25,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Backend Server starts running...`);
+server.listen(port, () => {
+  console.log("Server start on port 3000.");
 });
