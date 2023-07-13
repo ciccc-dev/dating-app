79746c078d0bc12bbf3520523df7ea61f8a77f92
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-07-13 23:47:19 +0900

    Creates message library

---
+++ backend/lib/messages/repository.ts
@@ -0,0 +1,9 @@
+export const fetchMessagesByRoom = () => {
+  return {
+    id: "111111111111111111",
+    messages: [
+      { time: "2023-07-13 23:34", user: "aaaaaaaaaaaaaaa", text: "Hello, Jay" },
+      { time: "2023-07-13 23:35", user: "bbbbbbbbbbbbbbb", text: "Hello, Ann" },
+    ],
+  };
+};
