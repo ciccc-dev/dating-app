59ef5248386df181bea5b9f2571f1b5166e22630
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-07-19 11:05:10 +0900

    Applys prisma type to chat seed

--- backend/prisma/seeds/chats.ts
+++ backend/prisma/seeds/chats.ts
@@ -1,42 +1,36 @@
 import crypto from "crypto";
 
 import { PrismaClient } from "@prisma/client";
-
-interface Chat {
-  id: string;
-  sentBy: string;
-  receivedBy: string;
-  message: string;
-  hasRead: boolean;
-  timestamp: Date;
-}
+import { Prisma } from "@prisma/client";
 
 export const seedChats = async (prisma: PrismaClient) => {
-  const chats: Chat[] = [
-    {
-      id: crypto.randomUUID(),
-      sentBy: "auth0|6493c3668860a0c976f765af",
-      message: "Hello",
-      receivedBy: "auth0|64af99336e86aeb92a526d0e",
-      hasRead: false,
-      timestamp: new Date(),
-    },
-    {
-      id: crypto.randomUUID(),
-      sentBy: "auth0|64af99336e86aeb92a526d0e",
-      message: "Hey",
-      receivedBy: "auth0|6493c3668860a0c976f765af",
-      hasRead: false,
-      timestamp: new Date(),
-    },
-    {
-      id: crypto.randomUUID(),
-      sentBy: "auth0|64af99336e86aeb92a526d0e",
-      message: "test",
-      receivedBy: "auth0|64b485fd37c9277946e4b7b9",
-      hasRead: false,
-      timestamp: new Date(),
-    },
-  ];
-  await prisma.chat.createMany({ data: chats });
+  const chats: Prisma.ChatCreateManyArgs = {
+    data: [
+      {
+        id: crypto.randomUUID(),
+        sentBy: "auth0|6493c3668860a0c976f765af",
+        message: "Hello",
+        receivedBy: "auth0|64af99336e86aeb92a526d0e",
+        hasRead: false,
+        timestamp: new Date(),
+      },
+      {
+        id: crypto.randomUUID(),
+        sentBy: "auth0|64af99336e86aeb92a526d0e",
+        message: "Hey",
+        receivedBy: "auth0|6493c3668860a0c976f765af",
+        hasRead: false,
+        timestamp: new Date(),
+      },
+      {
+        id: crypto.randomUUID(),
+        sentBy: "auth0|64af99336e86aeb92a526d0e",
+        message: "test",
+        receivedBy: "auth0|64b485fd37c9277946e4b7b9",
+        hasRead: false,
+        timestamp: new Date(),
+      },
+    ],
+  };
+  await prisma.chat.createMany(chats);
 };
