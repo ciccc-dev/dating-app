ad2575d0beabce82b4d3508e9f1f7f9a65350e11
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-07-19 10:32:29 +0900

    Adds logic to fetch messages and return via websocket

--- backend/lib/messages/repository.ts
+++ backend/lib/messages/repository.ts
@@ -1,4 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+
 import { allMessages } from "./testdata";
+
+const prisma = new PrismaClient();
 
 export const fetchMessagesByRoomId = (roomId: string) => {
   return allMessages.filter((message) => message.roomId === roomId);
@@ -15,4 +19,18 @@ export const fetchMessagesByRoomIds = (roomdIds: string[]) => {
 
 export const fetchAllMessages = () => {
   return allMessages;
+};
+
+export const fetchMessagesByUserId = async (userId: string) => {
+  const messages = await prisma.chat.findMany({
+    include: { sender: true, receiver: true },
+    where: { OR: [{ sentBy: userId }, { receivedBy: userId }] },
+  });
+  const rawPartners = messages.map((message) =>
+    message.sentBy === userId ? message.receiver : message.sender
+  );
+  const partners = rawPartners.filter((partner, index, self) => {
+    return index === self.findIndex((p) => p.id === partner.id);
+  });
+  return { partners, messages };
 };
