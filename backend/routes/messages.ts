abec3a77eb2fd0087fc31bbbc9a58ed94b9c402e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-07-21 22:36:34 +0900

    Chore

--- backend/routes/messages.ts
+++ backend/routes/messages.ts
@@ -5,9 +5,13 @@ import { MessageRepository } from "../lib/messages/repository";
 const router = express.Router();
 
 router.get("/:userId", async (req, res) => {
-  const { partners, messages } = await MessageRepository.fetchMessagesByUserId(
+  const messages = await MessageRepository.fetchMessagesByUserId(
     req.params.userId as string
   );
+  const partners = await MessageRepository.fetchPartnersByUserId(
+    req.params.userId as string
+  );
+
   res.json({ partners, messages });
 });
 
