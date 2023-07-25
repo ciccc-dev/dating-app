2b059cd75b468fd8c62d7c388044029e1679710e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-07-25 16:49:25 +0900

    Seperates js files from html

--- frontend/public/404.js
+++ frontend/public/404.js
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
-
-    <title>SpendShare 404</title>
-    <!-- <script type="text/javascript">
         // Single Page Apps for GitHub Pages
         // https://github.com/rafrex/spa-github-pages
         // Copyright (c) 2016 Rafael Pedicini, licensed under the MIT License
@@ -41,11 +27,3 @@
             (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
             l.hash
         );
-    </script> -->
-    <script src="./404.js"></script>
-</head>
-
-<body>
-</body>
-
-</html>
