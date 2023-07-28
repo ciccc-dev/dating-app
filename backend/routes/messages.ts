aa4ebb5d8bed78f4cea7d6222a74108d6acee193
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-07-28 16:36:02 +0900

    Delete unnecessary lines

--- backend/routes/messages.ts
+++ backend/routes/messages.ts
@@ -7,9 +7,6 @@ const router = express.Router();
 
 router.get("/", getMessages);
 router.get("/:userId", getSpecificMessages);
-// Validate request
-// Request body should have message
-// message field is required
 router.post("/:userId", body("message").notEmpty(), postMessage);
 
 export default router;
