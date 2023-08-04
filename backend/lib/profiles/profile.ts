93d1ac5904113c718a9d88257943733bea61584b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-08-04 13:55:37 +0900

    Adds id field to Profile

--- backend/lib/profiles/profile.ts
+++ backend/lib/profiles/profile.ts
@@ -1,7 +1,11 @@
+import crypto from "crypto";
+import parseISO from "date-fns/parseISO";
+
 import { Gender, GENDER } from "../../constants";
 import { SexualOrientation } from "../../constants";
 
 export class Profile {
+  private _id: string;
   private _username: string;
   private _gender?: Gender;
   private _birthday: Date;
@@ -9,28 +13,32 @@ export class Profile {
   private _aboutMe: string;
 
   constructor({
+    id,
     username,
     gender,
     birthday,
     sexualOrientation,
     aboutMe,
   }: {
+    id: string | undefined;
     username: string;
     gender: Gender | undefined;
-    birthday: Date;
+    birthday: string;
     sexualOrientation: SexualOrientation | undefined;
     aboutMe: string;
   }) {
     if (gender && !Object.values(GENDER).includes(gender))
       throw "Gender is invalida";
 
+    this._id = id ?? crypto.randomUUID();
     this._username = username;
     this._gender = gender;
-    this._birthday = birthday;
+    this._birthday = parseISO(birthday);
     this._sexualOrientation = sexualOrientation;
     this._aboutMe = aboutMe ?? "";
   }
 
+  id = () => this._id;
   username = () => this._username;
   gender = () => this._gender;
   birthday = () => this._birthday;
