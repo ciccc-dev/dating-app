8cdefd9f9a2d48d34501a1637cd09b8e49d71f8f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-08-08 01:32:49 +0900

    Creates new constants for gender

---
+++ frontend/src/constants/gender.ts
@@ -0,0 +1,11 @@
+export const genders = {
+  MAN: "Man",
+  WOMAN: "Woman",
+  AGENDER: "Agender",
+  ANDROGYNE: "Androgyne",
+  ANDROGYNOUS: "Androgynous",
+  BIGENDER: "Bigender",
+  FEMALE_TO_MALE: "Female To Gender",
+  GENDER_FLUID: "Gender Fluid",
+  OTHER: "Don't want to answer",
+};
