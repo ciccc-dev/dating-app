6ec408602fb90be0915317cdd07986fa13351abe
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-08-08 08:06:28 +0900

    Adds function and name value to username component

--- frontend/src/features/Signup/components/Username/index.tsx
+++ frontend/src/features/Signup/components/Username/index.tsx
@@ -1,37 +1,71 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import { InputLabel, Input, styled } from "@mui/material";
+import { styled } from "@mui/material";
 
-export const Username = () => {
+import { useDialogState } from "../../../../hooks/useDialogState";
+import { NotificationBar } from "../../../../components/NotificationBar";
+
+type NextPhase = "gender";
+
+interface Props {
+  name: string;
+  onChange: (key: string, value: string) => void;
+  onChangePhase: (value: NextPhase) => void;
+}
+
+export const Username = ({ name, onChange, onChangePhase }: Props) => {
+  const [isOpen, { open, close }] = useDialogState();
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    onChange("username", event.currentTarget.value);
+
+  const handleNext = () => {
+    if (name.length === 0) return open();
+    onChangePhase("gender");
+  };
+
   return (
-    <Box sx={{ justifyContent: "center", width: "100%" }}>
+    <StyledWrapper>
       <Wrapper>
         <Typography variant='h2' gutterBottom align='center'>
           My name is
         </Typography>
       </Wrapper>
       <Wrapper>
-        <FormControl fullWidth>
-          <InputLabel size='normal'>USERNAME</InputLabel>
-          <Input />
-        </FormControl>
+        <StyledTextField
+          label='USERNAME'
+          variant='standard'
+          value={name}
+          onChange={handleChange}
+        />
       </Wrapper>
-      <Wrapper
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <Button variant='contained'>Next</Button>
+      <Wrapper>
+        <Button variant='contained' onClick={handleNext}>
+          Next
+        </Button>
       </Wrapper>
-    </Box>
+      <NotificationBar
+        isOpen={isOpen}
+        onClose={close}
+        isSuccess={false}
+        message='Please input your username'
+      />
+    </StyledWrapper>
   );
 };
+
+const StyledWrapper = styled(Box)`
+  justify-content: center;
+  width: 100%;
+`;
 
 const Wrapper = styled(`div`)`
   margin: 100px;
   display: flex;
   justify-content: center;
+`;
+
+const StyledTextField = styled(TextField)`
+  width: 50%;
 `;
