772c4438bddba58a7eff7fcf6255dd20b76babc5
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-08-08 20:52:24 +0900

    Adds varialble and function to birthday component

--- frontend/src/features/Signup/components/Birthday/index.tsx
+++ frontend/src/features/Signup/components/Birthday/index.tsx
@@ -1,10 +1,27 @@
+import { ChangeEvent } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import { InputLabel, Input, styled } from "@mui/material";
+import { styled } from "@mui/material";
 
-export const Birthday = () => {
+type Phase = "gender" | "showMe";
+
+interface Props {
+  value: string;
+  onChange: (category: string, birthday: string) => void;
+  onChangePhase: (phase: Phase) => void;
+}
+
+export const Birthday = ({ value, onChange, onChangePhase }: Props) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
+    onChange("birthday", event.currentTarget.value as string);
+
+  const navigatePrev = () => onChangePhase("gender");
+  const navigateNext = () => {
+    if (value.length > 0) onChangePhase("showMe");
+  };
+
   return (
     <StyledWrapper>
       <Wrapper>
@@ -13,10 +30,12 @@ export const Birthday = () => {
         </Typography>
       </Wrapper>
       <Wrapper>
-        <StyledFromControl>
-          <InputLabel size='normal'>YYYY / MM / DD</InputLabel>
-          <Input />
-        </StyledFromControl>
+        <StyledTextField
+          label='YYYY / MM / DD'
+          variant='standard'
+          value={value}
+          onChange={handleChange}
+        />
       </Wrapper>
       <Wrapper
         style={{
@@ -24,20 +43,16 @@ export const Birthday = () => {
           justifyContent: "center",
         }}
       >
-        <Button variant='outlined' sx={{ margin: 1 }}>
+        <Button variant='outlined' sx={{ margin: 1 }} onClick={navigatePrev}>
           Back
         </Button>
-        <Button variant='contained' sx={{ margin: 1 }}>
+        <Button variant='contained' sx={{ margin: 1 }} onClick={navigateNext}>
           Next
         </Button>
       </Wrapper>
     </StyledWrapper>
   );
 };
-
-const StyledFromControl = styled(FormControl)`
-  width: 50%;
-`;
 
 const StyledWrapper = styled(Box)`
   justify-content: center;
@@ -48,4 +63,8 @@ const Wrapper = styled(`div`)`
   margin: 100px;
   display: flex;
   justify-content: center;
+`;
+
+const StyledTextField = styled(TextField)`
+  width: 50%;
 `;
