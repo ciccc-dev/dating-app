6bdd98d3e5098f69204a7852da6e8ef7a645adce
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-08-15 12:43:11 +0900

    Erase unnecessary page

--- frontend/src/pages/DatingApp.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, useOutletContext } from "react-router-dom";
-import { DatingAppWrapper } from "../features/DatingApp/components/Wrapper";
-
-export const DatingApp = () => {
-  const user = useOutletContext();
-  console.log("@@@@@@@@@@@@@", user);
-
-  return <DatingAppWrapper Outlet={Outlet} />;
-};
