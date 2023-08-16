6b9bff0a57a0630b8c153d5e6acab8fb46044d03
AkitoTobita <t0bitakito.001@gmail.com>
2023-08-16 16:55:56 -0700

    update: added choose button and upload button in a component

--- frontend/src/features/Account/components/ProfilePhotos/index.tsx
+++ frontend/src/features/Account/components/ProfilePhotos/index.tsx
@@ -1,6 +1,13 @@
-import { Grid, styled } from "@mui/material";
+import { Stack, Box, Button, Grid, styled } from "@mui/material";
+import { useState } from "react";
+
+export interface IFile {
+  url: string;
+  name: string;
+}
 
 export const ProfilePhotos = () => {
+  const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
   const photos = [
     {
       id: "001",
@@ -29,22 +36,68 @@ export const ProfilePhotos = () => {
     },
   ];
 
+  const selectImages = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let images: Array<string> = [];
+    let files = event.target.files;
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        images.push(URL.createObjectURL(files[i]));
+      }
+      setSelectedFiles(files);
+    }
+  };
+
   return (
     <>
       <StyledGrid container spacing={0}>
-        {photos.map((photo) => (
+        {photos.map((photo, index) => (
           <Grid key={photo.id} item xs={photo.sortOrder === 1 ? 12 : 3}>
-            <StyleImg
-              src={photo.photoUrl}
-              alt={`userPhoto-${photo.id}`}
+            <Box
+              key={index}
               sx={{
-                width: photo.sortOrder === 1 ? "300px" : "75px",
-                height: photo.sortOrder === 1 ? "400px" : "100px",
+                width: index === 0 ? "300px" : "75px",
+                height: index === 0 ? "400px" : "100px",
+                background: "grey",
               }}
-            />
+            >
+              {selectedFiles && selectedFiles[index] ? (
+                <StyleImg
+                  src={URL.createObjectURL(selectedFiles[index])}
+                  alt={`userPhoto-${index}`}
+                  sx={{
+                    width: photo.sortOrder === 1 ? "300px" : "75px",
+                    height: photo.sortOrder === 1 ? "400px" : "100px",
+                  }}
+                />
+              ) : null}
+            </Box>
           </Grid>
         ))}
       </StyledGrid>
+      <Box sx={{ marginTop: "1rem" }}>
+        <Stack direction="row" alignItems="center" spacing={2}>
+          <Button
+            variant="outlined"
+            component="label"
+            sx={{ width: "100%", fontSize: "1.2rem", marginBottom: "1rem" }}
+          >
+            Choose Photos
+            <input
+              hidden
+              accept="image/*"
+              multiple
+              type="file"
+              onChange={selectImages}
+            />
+          </Button>
+        </Stack>
+        <Button
+          variant="outlined"
+          sx={{ width: "100%", fontSize: "1.2rem", marginBottom: "1rem" }}
+        >
+          Upload
+        </Button>
+      </Box>
     </>
   );
 };
