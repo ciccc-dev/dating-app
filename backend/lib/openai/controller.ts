efad15e2478c2b4a37a8cb2e945599b954811a5f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-08-22 17:21:16 +0900

    Adds information which Chatgpt will read and understand

--- backend/lib/openai/controller.ts
+++ backend/lib/openai/controller.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from "express";
 import { OpenAiRepository } from "./repository";
 import { ProfileRepository } from "../profiles";
 import { validate } from "../../middleware/validateRequest";
+import { Completion } from "./completion";
 
 export const getAnswersGeneratedByAi = async (
   req: Request,
@@ -17,11 +18,12 @@ export const getAnswersGeneratedByAi = async (
     );
     const question = req.query.question as string;
 
-    const generatedText = await OpenAiRepository.getAnswersGeneratedByAi({
-      profile,
-      question,
-    });
-    res.status(200).json(generatedText);
+    const completion: Completion =
+      await OpenAiRepository.getAnswersGeneratedByAi({
+        profile,
+        question,
+      });
+    res.status(200).json({ reply: completion.content() });
   } catch (err) {
     next(err);
   }
--- backend/lib/openai/repository.ts
+++ backend/lib/openai/repository.ts
@@ -1,4 +1,6 @@
 import OpenAI from "openai";
+import { calculateAge } from "../../utils/caluculateAge";
+import { Completion } from "./completion";
 
 class _OpenAiRepository {
   private openai: OpenAI;
@@ -22,38 +24,54 @@ class _OpenAiRepository {
     question,
   }: {
     profile?: any;
+    // profile?: {
+    //   gender: string;
+    //   username: string;
+    //   purposes: { name: string }[];
+    //   interests: { name: string }[];
+    // };
     question: string;
   }) => {
     const profilePrompt = profile
-      ? `You are ${profile.gender}. Your name is ${profile.username}.`
+      ? `You are ${profile.gender}.` +
+        ` Your name is ${profile.userName}.` +
+        ` You are ${calculateAge(profile.birthday)} years old` +
+        ` Your sexual orientation is ${profile.sexualOrientation}.`
       : "";
+    const aboutMePrompt =
+      profile && profile.aboutMe.length > 0
+        ? `This is your explanation: ${profile.aboutMe}.`
+        : "";
     const purposesPrompt =
-      profile.purposes.length > 0
+      profile && profile.purposes.length > 0
         ? `You are looking for ${profile.purposes
             .map((purpose: { name: string }) => purpose.name)
             .toString()}.`
         : "";
     const interestsPrompt =
-      profile.interests.length > 0
+      profile && profile.interests.length > 0
         ? `You are interested in ${profile.interests
             .map((interest: { name: string }) => interest.name)
             .toString()}.`
         : "";
 
-    console.log(
-      `${profilePrompt} ${purposesPrompt} ${interestsPrompt} Please answer this question from provided profile information. ${question}`
-    );
+    const completion: OpenAI.Chat.Completions.ChatCompletion =
+      await this.openai.chat.completions.create({
+        messages: [
+          {
+            role: "system",
+            content: `${profilePrompt} ${purposesPrompt} ${interestsPrompt} ${aboutMePrompt} Please answer this question from provided profile information. ${question}`,
+          },
+        ],
+        model: this.gptModel,
+      });
 
-    const completion = await this.openai.chat.completions.create({
-      messages: [
-        {
-          role: "system",
-          content: `${profilePrompt} ${purposesPrompt} Please answer this question from provided profile information. ${question}`,
-        },
-      ],
-      model: this.gptModel,
+    return new Completion({
+      id: completion.id,
+      created: completion.created,
+      model: completion.model,
+      content: completion.choices[0].message.content,
     });
-    return completion.choices;
   };
 }
 
