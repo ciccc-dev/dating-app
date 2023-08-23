c00bb033383911fb75791951105c4b89e0e1fa6c
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-08-23 23:58:57 +0900

    Adds interests for repository

--- backend/lib/profiles/repository.ts
+++ backend/lib/profiles/repository.ts
@@ -204,7 +204,10 @@ class _ProfileRepository {
   };
 
   createProfile = async (profile: Profile, filter: Filter, userId: string) => {
-    const interest: Prisma.InterestCreateNestedManyWithoutProfilesInput = {};
+    const interests: any[] = profile.interests().map((interest) => {
+      return { id: interest.id };
+    });
+
     const data: Prisma.ProfileCreateInput = {
       id: profile.id(),
       userId,
@@ -222,7 +225,7 @@ class _ProfileRepository {
           purposes: filter.purposes(),
         },
       },
-      interests: {},
+      interests: { connect: interests },
     };
     return await this.db.profile.create<Prisma.ProfileCreateArgs>({ data });
   };
