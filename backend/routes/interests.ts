459621f7614169130cb8edd219bd8977df3189ef
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-08-23 21:53:37 +0900

    Creates interest route file

--- backend/routes/interests.ts
+++ backend/routes/interests.ts
@@ -1,17 +1,9 @@
-import { PrismaClient } from "@prisma/client";
 import express from "express";
+
+import { getInterests } from "../lib/interests";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const client = new PrismaClient();
-  const result = await client.interest.findMany({
-    orderBy: {
-      sortOrder: "asc",
-    },
-    select: { id: true, name: true },
-  });
-  return res.json(result);
-});
+router.get("/", getInterests);
 
 export default router;
